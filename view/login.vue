e0846a45a4db166a9306d4425cd82ba89df80430
hsieh435 <hsieh435@gmail.com>
2025-06-18 00:05:16 +0800

    20250617

--- view/login.vue
+++ view/login.vue
@@ -25,7 +25,10 @@ const password = ref<string>("");
 const handleLogin = () => {
   console.log('登入資訊:', { userAccount: userAccount.value, password: password.value });
   // 在此加入 API 呼叫與驗證邏輯
+  console.log('登入成功！');
 };
+
+
 
 
 </script>
